c7b841502ddafa7a4dae46b0ce36f1a3f07b6e2f
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2020-01-12 18:32:55 +0100

    dd

--- scriptchart.js
+++ scriptchart.js
@@ -15,16 +15,16 @@ firebase.on('value', function(snapshot) {
 });
 
 function drawGraph(  light ){
-  var labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+
+  var labels = [ 00,02,04,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40];
   var ctx = document.getElementById("myChart").getContext('2d');
- // myChart = new Chart(ctx).Line(data, options);
   var myChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: labels,
     datasets: [   
                  {
-            label: "humidité",
+            label: "%  humidité chaque 2 min",
            labelString : "humidité",
 
     borderColor: 'rgb(145, 178, 253)',
@@ -53,7 +53,7 @@ function drawGraph(  light ){
                         id: "y-axis-temp",
                         ticks: {
                           beginAtZero:true,
-                          suggestedMax: 1500
+                          suggestedMax: 100
                         }
               
                     }],
